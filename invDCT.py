90a2fe63a057ebee64df3706f9c3a245604a8a8f
mira9272 <mira.92725684@gmail.com>
2021-09-12 12:20:47 +0900

    modify algorithm

--- invDCT.py
+++ invDCT.py
@@ -15,51 +15,43 @@ class InvDCT:
 
         N = self.Nx * self.Ny * self.Nz * self.Nt
 
-        F_matrix = np.zeros(N, N)
+        F_matrix = np.zeros((N, N))
 
-        for nx in range(self.Nx):
-            for ny in range(self.Ny):
-                for nz in range(self.Nz):
-                    for nt in range(self.Nt):
+        for nx in range(1, self.Nx + 1):
+            for ny in range(1, self.Ny + 1):
+                for nz in range(1, self.Nz + 1):
+                    for nt in range(1, self.Nt + 1):
 
-                        for kx in range(self.Nx):
-                            for ky in range(self.Ny):
-                                for kz in range(self.Nz):
-                                    for kt in range(self.Nt):
+                        for kx in range(1, self.Nx + 1):
+                            for ky in range(1, self.Ny + 1):
+                                for kz in range(1,self.Nz + 1):
+                                    for kt in range(1, self.Nt + 1):
 
                                         k = (kx - 1) * self.Ny * self.Nz * self.Nt + \
-                                            (ky - 1) * self.Nz * self.Nt * \
-                                            (kz - 1) * self.Nt * kt
+                                            (ky - 1) * self.Nz * self.Nt + \
+                                            (kz - 1) * self.Nt + kt
 
                                         n = (nx - 1) * self.Ny * self.Nz * self.Nt + \
-                                            (ny - 1) * self.Nz * self.Nt * \
-                                            (nz - 1) * self.Nt * nt
+                                            (ny - 1) * self.Nz * self.Nt + \
+                                            (nz - 1) * self.Nt + nt
 
-                                        if kx == 1:
-                                            a1 = 1 / math.sqrt(self.Nx)
-                                        else:
-                                            a1 = math.sqrt(2 / self.Nx)
+                                        a1 = 1 / math.sqrt(self.Nx) if kx == 1 else math.sqrt(2 / self.Nx)
 
-                                        if ky == 1:
-                                            a2 = 1 / math.sqrt(self.Ny)
-                                        else:
-                                            a2 = math.sqrt(2 / self.Ny)
+                                        a2 = 1 / math.sqrt(self.Ny) if ky == 1 else math.sqrt(2 / self.Ny)
+                                            
+                                        a3 = 1 / math.sqrt(self.Nz) if kz == 1 else math.sqrt(2 / self.Nz)
 
-                                        if kz == 1:
-                                            a3 = 1 / math.sqrt(self.Nz)
-                                        else:
-                                            a3 = math.sqrt(2 / self.Nz)
-
-                                        if kt == 1:
-                                            a4 = 1 / math.sqrt(self.Nt)
-                                        else:
-                                            a4 = math.sqrt(2 / self.Nt)
+                                        a4 = 1 / math.sqrt(self.Nt) if kt == 1 else math.sqrt(2 / self.Nt)
 
                                         
                                         F_matrix[n-1, k-1] = a1 * a2 * a3 * a4 *\
-                                            math.cos(math.pi * (2 * nx - 1) * (kx - 1) / (2 * self.Nx)) *\
-                                            math.cos(math.pi * (2 * ny - 1) * (ky - 1) / (2 * self.Ny)) *\
-                                            math.cos(math.pi * (2 * nz - 1) * (kz - 1) / (2 * self.Nz)) *\
-                                            math.cos(math.pi * (2 * nt - 1) * (kt - 1) / (2 * self.Nt))
-
+                                                math.cos(math.pi * (2 * nx - 1) * (kx - 1) / (2 * self.Nx)) *\
+                                                math.cos(math.pi * (2 * ny - 1) * (ky - 1) / (2 * self.Ny)) *\
+                                                math.cos(math.pi * (2 * nz - 1) * (kz - 1) / (2 * self.Nz)) *\
+                                                math.cos(math.pi * (2 * nt - 1) * (kt - 1) / (2 * self.Nt))
         return F_matrix
+# %%
+sample = InvDCT(20,20)
+# %%
+X = sample.InvDCTmatrix()
+# %%
