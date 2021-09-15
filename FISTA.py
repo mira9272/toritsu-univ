2c72468b8c32e6c14c76ca913216516ab42ac05c
mira9272 <mira.92725684@gmail.com>
2021-09-15 13:02:28 +0900

    operation confirmed

--- FISTA.py
+++ FISTA.py
@@ -3,16 +3,21 @@
 import math
 import numpy as np
 
+    # 軟判定しきい値関数
+def soft_threashold(y, alpha):
+        return np.sign(y) * np.maximum(np.abs(y) - alpha, 0.0)
+
+
 def FISTA(map_A, estimate_y, opt_lambda):
 
     # 初期値の設定
     N = map_A.shape[1]
-    new_x = np.zeros((N, 1))
+    new_x = np.linalg.pinv(map_A) @ estimate_y
     new_w = np.zeros((N, 1))
     new_beta = 1
 
     # リプシッツ定数Lの計算
-    L = np.linalg.eigvals(np.array(map_A).T * map_A)
+    L = np.linalg.eigvals(np.array(map_A).T @ map_A)
     Lipschitz_L = np.linalg.norm(L, ord = 2) / opt_lambda
 
     # FISTAのアルゴリズム
@@ -24,28 +29,21 @@ def FISTA(map_A, estimate_y, opt_lambda):
         init_beta = new_beta
 
         # LASSO
-        output_v = init_x + np.array(map_A).T * (estimate_y - map_A * init_w) / Lipschitz_L * opt_lambda
+        output_v = init_x + np.array(map_A).T @ (estimate_y - map_A @ init_w) / Lipschitz_L * opt_lambda
 
         # 軟判定しきい値関数
         new_x = soft_threashold(output_v, 1 / Lipschitz_L)
 
         # ループ終了判定
         if np.linalg.norm(new_x - init_x) < 1E-6:
-            
+            new_x += 0
+
             return new_x
 
-        # 高速化部分
-        new_beta = (1 + math.sqrt(1 + 4 * (init_beta) * (init_beta))) / 2
-        new_w = new_x + (init_beta - 1) * (new_x - init_x) / new_beta
-#%%
-    # 軟判定しきい値関数
-def soft_threashold(y, alpha):
-        return np.sign(y) * np.maximum(np.abs(y) - alpha, 0.0)
+        else:
 
-#%%
-import math
-import numpy as np
-import numpy.linalg as LA
-# %%
-x = np.array([1, 2, 3])
+            # 高速化
+            new_beta = (1 + math.sqrt(1 + 4 * (init_beta) ** 2)) / 2
+            new_w = new_x + (init_beta - 1) * (new_x - init_x) / new_beta
+
 # %%
